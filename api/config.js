96fb241b8133dcbf8964a46c378ab68a883cd965
Abdalhafeedh Omer <abdalhafeddhomer@gmail.com>
2026-02-04 13:58:40 +0800

    Add config endpoint for Vercel

---
+++ api/config.js
@@ -0,0 +1,16 @@
+// Config Endpoint - Vercel Serverless Function
+
+export default function handler(req, res) {
+    // Set CORS headers
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+    if (req.method === 'OPTIONS') {
+        return res.status(200).end();
+    }
+
+    res.status(200).json({
+        clientId: process.env.GOOGLE_CLIENT_ID
+    });
+}
